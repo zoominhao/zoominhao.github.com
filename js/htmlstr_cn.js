7471105910ec44617029be80d07ac14f1b0cd640
zoominhao <tencenthao@gmail.com>
2014-11-06 11:36:16 +0800

    finish Chinese content

---
+++ js/htmlstr_cn.js
@@ -0,0 +1,107 @@
+var procontent = new Array();
+procontent[0] = "<ul>"+
+               	"<li>"+
+                "<div class='left'>"+
+                "<a><img src='images/gallery/onlineexam_a.jpg' alt='' /></a>"+
+                "</div>"+
+                "<div class='right'>"+
+                "<h5>学院萌芽项目</h5>"+
+                "<p>四川大学软件学院在线考试系统,主要是基于经典的web MVC的三层架构，MySQL数据库和JAVA语言。<a href='http://swjx.scu.edu.cn:8001/onlineexam/' target='_blank'>链接</a> .</p>"+
+                "<div class='button'><a href='#'>查看详细 <span>&raquo;</span></a> </div>"+
+                "</div>"+
+                "<div class='cleaner'></div>"+
+                "</li>"+
+                "<li>"+
+                "<div class='left'>"+
+                "<a><img src='images/gallery/innovation_a.jpg' alt='' /></a>"+
+                "</div>"+
+                "<div class='right'>"+
+                "<h5>全国大学生创新大赛</h5>"+
+                "<p>开发基于Zigbee网络的物联网技术的智慧校园系统，获得了全国二等奖。系统包含了资源监控，防盗，防火以及人员定位等等功能。基于C/C++，ActionScript，Java 和 JS。</p>"+
+                "<div class='button'><a href='#'>查看详细 <span>&raquo;</span></a> </div>"+
+                "</div>"+
+               	"<div class='cleaner'></div>"+
+                "</li>"+
+				"<li>"+
+                "<div class='left'>"+
+                "<a><img src='images/gallery/innerDB_a.jpg' alt='' /></a>"+
+                "</div>"+
+                "<div class='right'>"+
+                "<h5>全国大学生创新实验计划</h5>"+
+                "<p>共享内存的数据库(SHMDB)，主要部分： 数据结构，访问方法，通信协议，并发控制，失败恢复等等。 我的主要工作是设计数据结构，设计基于QT的用户界面，以及主要编码工作。</p>"+
+                "<div class='button'><a href='#'>查看详细 <span>&raquo;</span></a> </div>"+
+                "</div>"+
+                "<div class='cleaner'></div>"+
+                "</li>"+
+                "</ul>";
+procontent[1] = "<ul>"+
+               	"<li>"+
+                "<div class='left'>"+
+                "<a><img src='images/gallery/synopsis_a.jpg' alt='' /></a>"+
+                "</div>"+
+                "<div class='right'>"+
+                "<h5>三维摘要</h5>"+
+                "<p>在本工作中我们提出了一个理想的三维路径可视化工具，旨在自动生成预定义时间长度的游览摘要，同时尽可能保证信息量和有趣性。用到了 OSG(OPENGL),CGAL,OPENCV,ENGIEN 等公用库。计划投稿2014的EuroGraph会议。</br>&nbsp<a href='https://www.youtube.com/watch?v=R08hMiccxzg' target='_blank'>3DVideo1</a> &nbsp<a href='https://www.youtube.com/watch?v=GFZCrc1qOlo' target='_blank'>3DVideo2</a>&nbsp<a href='https://www.youtube.com/watch?v=XeSLJsh7lj0' target='_blank'>SVVideo1</a>&nbsp<a href='https://www.youtube.com/watch?v=yhQkHajNpJA' target='_blank'>SVVideo2</a> </p>"+
+                "<div class='button'><a href='#'>查看详细 <span>&raquo;</span></a> </div>"+
+                "</div>"+
+                "<div class='cleaner'></div>"+
+                "</li>"+
+                "<li>"+
+                "<div class='left'>"+
+                "<a><img src='images/gallery/sym_a.jpg' alt='' /></a>"+
+                "</div>"+
+                "<div class='right'>"+
+                "<h5>内蕴对称</h5>"+
+                "<p>加强形状自对称是一个很基础的美学价值观问题。本工作中，主要致力于还原模型内蕴对称的美学感，亦即同时保证形状对称以及其特定的动态姿势。 投稿于2014年Siggraph Asia。</p>"+
+                "<div class='button'><a href='#'>查看详细 <span>&raquo;</span></a> </div>"+
+                "</div>"+
+               	"<div class='cleaner'></div>"+
+                "</li>"+
+				"<li>"+
+                "<div class='left'>"+
+                "<a><img src='images/gallery/lg_a.jpg' alt='' /></a>"+
+                "</div>"+
+                "<div class='right'>"+
+                "<h5>谷歌开源项目，”银河系“系统</h5>"+
+                "<p>我在<a href='https://code.google.com/p/liquid-galaxy/' target='_blank'>谷歌夏令营项目</a> 中主要是做多屏幕视角同步(2012 5000$)(<a href='http://210.75.252.103:9500/display/?master=true' target='_blank'>GSOC</a>) 和集成中国街景 (2014 5500$)。 基于JS尤其是Node.js。 <a href='https://www.youtube.com/watch?v=ZPf4y-q93b0' target='_blank'>SyVideo1</a>&nbsp<a href='https://www.youtube.com/watch?v=OMQ4xFjJeUI' target='_blank'>SyVideo2</a>&nbsp<a href='https://www.youtube.com/watch?v=04Rn_Wrlb-A' target='_blank'>SyVideo3</a></p>"+
+                "<div class='button'><a href='#'>查看详细 <span>&raquo;</span></a> </div>"+
+                "</div>"+
+                "<div class='cleaner'></div>"+
+                "</li>"+
+                "</ul>";
+procontent[2] = "<ul>"+
+               	"<li>"+
+                "<div class='left'>"+
+                "<a><img src='images/gallery/baidu_a.jpg' alt='' /></a>"+
+                "</div>"+
+                "<div class='right'>"+
+                "<h5>百度, LBS VA, 北京</h5>"+
+                "<p>开发自动分析“数据在线处理”质量以及用户满意度的框架。基于PHP和C++。</p>"+
+                "<div class='button'><a href='#'>查看详细 <span>&raquo;</span></a> </div>"+
+                "</div>"+
+                "<div class='cleaner'></div>"+
+                "</li>"+
+                "<li>"+
+                "<div class='left'>"+
+                "<a><img src='images/gallery/qq_a.jpg' alt='' /></a>"+
+                "</div>"+
+                "<div class='right'>"+
+                "<h5>腾讯, MIG, 成都</h5>"+
+                "<p>开发移动设备网络服务和开发商移动软件上传系统（检查软件质量）。基于BJava，JSP 和 JS。</p>"+
+                "<div class='button'><a href='#'>查看详细 <span>&raquo;</span></a> </div>"+
+                "</div>"+
+               	"<div class='cleaner'></div>"+
+                "</li>"+
+				"<li>"+
+                "<div class='left'>"+
+                "<a><img src='images/gallery/baveda_a.jpg' alt='' /></a>"+
+                "</div>"+
+                "<div class='right'>"+
+                "<h5>深圳百维达公司街景项目</h5>"+
+                "<p>基于<a href='http://baveda.com/panorama.html' target='_blank'>深圳全景图像</a>的研究和开发。给用户提供一个基于网络的沉浸式体验系统。产品已经集成到许多在线系统，比如<a href='http://www.chachaba.com/' target='_blank'>查查吧</a>,<a href='http://www.e511.com/' target='_blank'>易行网</a>,深圳 RTA ,深圳市城管系统 和<a href='https://www.youtube.com/watch?v=cLLORSllYuQ' target='_blank'> 虚拟驾驶的应用</a>.</p>"+
+                "<div class='button'><a href='#'>查看详细 <span>&raquo;</span></a> </div>"+
+                "</div>"+
+                "<div class='cleaner'></div>"+
+                "</li>"+
+                "</ul>";				
+
